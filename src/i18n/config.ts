4f29cbcb7a33f91b47103d66344c7bfd84fa8a35
Cristian Londono <crisdevenginer@gmail.com>
2026-09-09 14:36:20 -0500

    feat: integrate i18n support and configurations
    Add i18next configuration and translation files (en.json, es.json) to support English and Spanish natively across the application.

---
+++ src/i18n/config.ts
@@ -0,0 +1,20 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import enTranslations from './en.json';
+import esTranslations from './es.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: enTranslations },
+      es: { translation: esTranslations },
+    },
+    lng: 'es', // Set Spanish as default as the user wrote in Spanish
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
